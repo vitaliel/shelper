78c2269673e43eb5777baee3fe2cba64f9a7a579
Vitalie Lazu <vitalie.lazu@gmail.com>
2008-11-24 12:45:20 +0200

    Added ejabberd plugin to add users

---
+++ plugins/os/ejabberd.rb
@@ -0,0 +1,32 @@
+#
+# Date: Mon, 24 Nov 2008 12:29:52 +0200
+#
+
+module Os
+  # Replacement for nagios-statd-server
+  class Ejabberd < SHelper::BasePlugin
+    name "ejabberd"
+    description "plugin to register users on ejabberd service."
+    author 'Vitalie Lazu'
+
+    cmd_map \
+    /^:ej\s+add\s+([a-z0-9]+)\s+([a-z0-9.-]+)/ => :ej_register
+
+    def help
+      rez = ""
+      rez << ":ej add USERNAME DOMAIN - will register a new user with random password\n"
+    end
+
+    # Parameters: username, domain
+    def ej_register(msg)
+      chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890-_"
+      password = ""
+      12.times { password << chars[rand(chars.length)] }
+
+      run_cmd "ejabberdctl register #{msg[1]} #{msg[2]} #{password}"
+      send_response("Password: " << password)
+    end
+  end
+end
+
+SHelper.register_plugin Os::Ejabberd
